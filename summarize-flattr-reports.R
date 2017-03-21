953b465e608abd323d065df06d4ab798f62a4486
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-03-21 19:27:37 +0100

    let RStudio fix whitespaces

--- summarize-flattr-reports.R
+++ summarize-flattr-reports.R
@@ -9,21 +9,19 @@ raw <- readr::read_csv2("Summarize-Flattr-Reports/data/KonScience-fflattr-revenu
 # append 1st days to months & convert to date format; learned from http://stackoverflow.com/a/4594269
 raw$period <- lubridate::as_date(paste0(raw$period, "-01"), format = "%Y-%m-%d")
 
-# summarize & export revenue per month
+# summarize & export revenue per month with trend
 plyr::ddply(raw,
-                   "period",
-                   plyr::summarize,
-                   all_clicks = sum(clicks),
-                   all_revenue = sum(revenue)) %>%
-
-# total revenue per month with trend
+            "period",
+            plyr::summarize,
+            all_clicks = sum(clicks),
+            all_revenue = sum(revenue)) %>%
   ggplot(aes(x = period, y = all_revenue, size = all_revenue)) +
-             geom_point(colour = "#ED8C3B")  +
-             stat_smooth(data = per_month, method = "auto", color = "#80B04A")  +  # fit trend plus confidence interval
-             scale_x_date(expand = c(0, 0), labels = scales::date_format("%Y-%b"))  +
-             scale_y_continuous(limits = c(0, max(per_month$all_revenue) * 1.1), expand = c(0, 0))  +
-             labs(title = "Development of Flattr Revenue", x = NULL, y = "EUR received")  +
-             theme(axis.text.x = element_text(angle = 15), legend.position = "none") ->
-monthly_simple_plot
+  geom_point(colour = "#ED8C3B")  +
+  stat_smooth(data = per_month, method = "auto", color = "#80B04A")  +  # fit trend plus confidence interval
+  scale_x_date(expand = c(0, 0), labels = scales::date_format("%Y-%b"))  +
+  scale_y_continuous(limits = c(0, max(per_month$all_revenue) * 1.1), expand = c(0, 0))  +
+  labs(title = "Development of Flattr Revenue", x = NULL, y = "EUR received")  +
+  theme(axis.text.x = element_text(angle = 15), legend.position = "none") ->
+  monthly_simple_plot
 
 plotly::ggplotly(monthly_simple_plot)
