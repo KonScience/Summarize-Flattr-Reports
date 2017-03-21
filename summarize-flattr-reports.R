90232af3a05a04836e4092721b4d606c52d15de2
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-03-21 19:22:56 +0100

    convert to pipe with right-assign

--- summarize-flattr-reports.R
+++ summarize-flattr-reports.R
@@ -2,6 +2,7 @@
 Sys.setlocale("LC_ALL", "UTF-8")
 
 library(ggplot2)
+library(magrittr)
 
 raw <- readr::read_csv2("Summarize-Flattr-Reports/data/KonScience-fflattr-revenue.csv")
 
@@ -9,20 +10,20 @@ raw <- readr::read_csv2("Summarize-Flattr-Reports/data/KonScience-fflattr-revenu
 raw$period <- lubridate::as_date(paste0(raw$period, "-01"), format = "%Y-%m-%d")
 
 # summarize & export revenue per month
-per_month <- plyr::ddply(raw,
+plyr::ddply(raw,
                    "period",
                    plyr::summarize,
                    all_clicks = sum(clicks),
-                   all_revenue = sum(revenue))
+                   all_revenue = sum(revenue)) %>%
 
 # total revenue per month with trend
-monthly_simple_plot <- ggplot(per_month, aes(x = period, y = all_revenue, size = all_revenue)) +
+  ggplot(aes(x = period, y = all_revenue, size = all_revenue)) +
              geom_point(colour = "#ED8C3B")  +
              stat_smooth(data = per_month, method = "auto", color = "#80B04A")  +  # fit trend plus confidence interval
              scale_x_date(expand = c(0, 0), labels = scales::date_format("%Y-%b"))  +
              scale_y_continuous(limits = c(0, max(per_month$all_revenue) * 1.1), expand = c(0, 0))  +
              labs(title = "Development of Flattr Revenue", x = NULL, y = "EUR received")  +
-             theme(axis.text.x = element_text(angle = 15), legend.position = "none")
+             theme(axis.text.x = element_text(angle = 15), legend.position = "none") ->
 monthly_simple_plot
 
 plotly::ggplotly(monthly_simple_plot)
