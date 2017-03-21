5ef2772e0f54096f6f95e0dc43013ff49ef5d9c5
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-03-21 20:27:31 +0100

    simplify theme

--- summarize-flattr-reports.R
+++ summarize-flattr-reports.R
@@ -21,10 +21,12 @@ plyr::ddply(raw,
   ggplot(aes(x = period, y = all_revenue, size = all_revenue)) +
   geom_point(colour = "#ED8C3B")  +
   stat_smooth(data = per_month, method = "auto", color = "#80B04A")  +  # fit trend plus confidence interval
-  scale_x_date(expand = c(0, 0), labels = scales::date_format("%Y-%b"))  +
-  scale_y_continuous(limits = c(0, max(per_month$all_revenue) * 1.1), expand = c(0, 0))  +
-  labs(title = "Development of Flattr Revenue", x = NULL, y = "EUR received")  +
-  theme(axis.text.x = element_text(angle = 15), legend.position = "none") ->
+  scale_x_date(labels = scales::date_format("%Y-%b"))  +
+  labs(title = "Flattr Revenue", x = NULL, y = "EUR received")  +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 15),
+        axis.ticks = element_line(),
+        legend.position = "none") ->
   monthly_simple_plot
 
 plotly::ggplotly(monthly_simple_plot)
